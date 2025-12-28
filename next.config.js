3b8e1abeda68979515bb3efcb33d14b83efce94a
hapsaboi <hapsahanis.hh@gmail.com>
2025-12-28 23:29:52 +0100

    BUG FIX

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  // Removed 'output: export' to support proper routing
+  // If you need static export, add trailingSlash: true instead
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
